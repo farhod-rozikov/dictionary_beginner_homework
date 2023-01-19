d58faf033ecca40bfccc64f9b8c513ad3357f3ce
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-19 19:31:42 +0500

    users name with age

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,17 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    return [user['name'] for user in data if user['age'] == age ]
+
+data = [
+  {
+    'name': 'John', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+]
+age = 27
+print(get_user_names_with_age(data, age))
