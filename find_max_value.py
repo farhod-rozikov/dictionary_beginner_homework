3a1f366bcb946d6d6355d2169de37e1ef06c9312
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-19 18:29:32 +0500

    find max value

--- find_max_value.py
+++ find_max_value.py
@@ -5,5 +5,11 @@ def find_max_value(data: dict):
         data (dict): A dictionary of values
     Returns:
         int: The maximum value in the dictionary.
-    """
-    return 
+    """ 
+    return max(data.values())
+data  = {
+    'a' : -4, 
+    'b' : -10, 
+    'c' : 0
+  }
+print(find_max_value(data))
