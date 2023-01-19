a2a7fdac0adcc800300ba6d86e28c091d8fbbe08
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-19 16:48:29 +0500

    length of values

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,7 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    v_len = 0
+    for v in data.values():
+        v_len += len(v)
+    return v_len
